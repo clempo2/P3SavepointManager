e619fbc1f1ae9a7965287c4bcf7595e6b45249c0
Clement Pellerin <clement_pellerin@hotmail.com>
2023-10-06 22:42:14 -0400

    initial implementation

---
+++ Assets/Scripts/Modes/SceneModes/P3SMAttractMode.cs
@@ -0,0 +1,484 @@
+using Multimorphic.NetProcMachine.Machine;
+using Multimorphic.P3;
+using Multimorphic.P3App.Data;
+using Multimorphic.P3App.Modes;
+using Multimorphic.P3App.Logging;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Multimorphic.P3App.GUI.Selector;
+using System;
+using Multimorphic.NetProc;
+
+namespace Multimorphic.P3SM.Modes
+{
+    public class P3SMAttractMode : AttractMode
+	{
+        // Names of menus
+        private const string STATE_ACTION = "Action";
+        private const string STATE_PROFILE = "Profile";
+        private const string STATE_APP = "App";
+        private const string STATE_SAVEPOINT = "Savepoint";
+        private const string STATE_NEWNAME = "NewName";
+
+        // Names of file operations
+        private const string ACTION_RENAME = "Rename Savepoint";
+        private const string ACTION_COPY   = "Copy Savepoint";
+        private const string ACTION_DELETE = "Delete Savepoint";
+
+        private const string BACKUP_EXTENSION = ".backup";
+
+        private string state;
+        private string action;
+        private string profileName;
+        private string appName;
+        private string savepointName;
+        private string newSavepointName;
+        private string backupPath;
+
+        // Need higher priority than ProfileManagerMode
+		public P3SMAttractMode (P3Controller controller, int priority, string SceneName)
+			: base(controller, 52111, SceneName)
+		{
+            AddModeEventHandler("Evt_ProfileSelectorResult", ProfileSelectorResultEventHandler, base.Priority);
+            AddModeEventHandler("Evt_ProfileSelectorCancelled", ProfileSelectorCancelledEventHandler, base.Priority);
+            AddModeEventHandler("Evt_ConfirmationBoxConfirm", ConfirmationBoxConfirmEventHandler, base.Priority);
+            AddModeEventHandler("Evt_ProfileNameEntryCompleted", ProfileNameEntryCompletedEventHandler, base.Priority);
+            AddModeEventHandler("Evt_ProfileNameEntryCancelled", ProfileNameEntryCancelledEventHandler, base.Priority);
+        }
+
+        public override void mode_started ()
+		{
+			base.mode_started ();
+
+            // Remove <None> and "Add Profile" from profile selection menu
+            GameAttribute addProfileAttr = data.GetGameAttribute("SelectAddProfileAllowed");
+            addProfileAttr.Set(false);
+            GameAttribute noneProfileAttr = data.GetGameAttribute("SelectNoneProfileAllowed");
+            noneProfileAttr.Set(false);
+        }
+
+        public override void mode_stopped()
+        {
+            RemoveModeEventHandler("Evt_ProfileSelectorResult", ProfileSelectorResultEventHandler, base.Priority);
+            RemoveModeEventHandler("Evt_ProfileSelectorCancelled", ProfileSelectorCancelledEventHandler, base.Priority);
+            RemoveModeEventHandler("Evt_ConfirmationBoxConfirm", ConfirmationBoxConfirmEventHandler, base.Priority);
+            RemoveModeEventHandler("Evt_ProfileNameEntryCompleted", ProfileNameEntryCompletedEventHandler, base.Priority);
+            RemoveModeEventHandler("Evt_ProfileNameEntryCancelled", ProfileNameEntryCancelledEventHandler, base.Priority);
+            base.mode_stopped();
+        }
+
+        private string GetProfilePath()
+        {
+            // Note: stock profiles under .../DefaultProfiles never have savepoints
+            // because they serve as seeds for new profiles, never as player profiles.
+
+            return DataManager.GetDefaultP3Path() + "PlayerProfiles" + Path.DirectorySeparatorChar + profileName;
+        }
+
+        private string GetSavedGamesPath()
+        {
+            return GetProfilePath() + Path.DirectorySeparatorChar + appName + Path.DirectorySeparatorChar + "SavedGames";
+        }
+
+        private string GetSavepointPath()
+        {
+            // Only valid after the Savepoint was selected
+            return GetSavedGamesPath() + Path.DirectorySeparatorChar + savepointName + ".gamestate";
+        }
+
+        private string GetNewSavepointPath()
+        {
+            // Only valid after the new savepoint name has been entered
+            return GetSavedGamesPath() + Path.DirectorySeparatorChar + newSavepointName + ".gamestate";
+        }
+
+        private List<string> GetSubdirectories(string parentPath)
+        {
+            DirectoryInfo parentDir = new DirectoryInfo(parentPath);
+            DirectoryInfo[] subDirs = parentDir.GetDirectories();
+            List<string> list = subDirs.Select(subDir => subDir.Name).ToList();
+            return list;
+        }
+
+        private List<string> GetFileNamesWithoutExtension(string parentPath, string searchPattern)
+        {
+            DirectoryInfo parentDir = new DirectoryInfo(parentPath);
+            List<string> list = new List<string>();
+            if (parentDir.Exists)
+            {
+                FileInfo[] files = parentDir.GetFiles(searchPattern);
+                list = files.Select(fileInfo => Path.GetFileNameWithoutExtension(fileInfo.Name)).ToList();
+            }
+            return list;
+        }
+
+        public override void SceneLiveEventHandler(string evtName, object evtData)
+        {
+            base.SceneLiveEventHandler(evtName, evtData);
+            PromptForAction();
+        }
+
+        private void PromptForAction()
+        {
+            action = null;
+            profileName = null;
+            appName = null;
+            savepointName = null;
+            newSavepointName = null;
+            backupPath = null;
+
+            state = STATE_ACTION;
+
+            List<string> list = new List<string>();
+            list.Add("Choose Action");
+            list.Add(ACTION_RENAME);
+            list.Add(ACTION_COPY);
+            list.Add(ACTION_DELETE);
+
+            SetSelectorData("ProfileSelector", list);
+            OpenDialog("ProfileDialog");
+        }
+
+        private void PromptForProfileName()
+        {
+            state = STATE_PROFILE;
+            PostModeEventToModes("Evt_ChooseProfile", false);
+        }
+
+        private void PromptForApp()
+        {
+            state = STATE_APP;
+
+            List<string> list = GetSubdirectories(GetProfilePath());
+            list.Insert(0, "Choose App");
+
+            SetSelectorData("ProfileSelector", list);
+            OpenDialog("ProfileDialog");
+        }
+
+        private void PromptForSavepoint()
+        {
+            state = STATE_SAVEPOINT;
+
+            List<string> list = GetFileNamesWithoutExtension(GetSavedGamesPath(), "*.gamestate");
+            if (list.Count == 0)
+            {
+                ShowPopup("No Savepoints", PromptForApp);
+                return;
+            }
+
+            list.Insert(0, "Choose Savepoint");
+
+            SetSelectorData("ProfileSelector", list);
+            OpenDialog("ProfileDialog");
+        }
+
+        private void PromptForNewName()
+        {
+            state = STATE_NEWNAME;
+            SetSelectorData("ProfileNameTextSelector", new TextSelectorData("Enter new savepoint", new List<string>(), ""));
+            OpenDialog("ProfileNameEditor");
+        }
+
+        private void PromptForOverwrite()
+        {
+            PromptForConfirmation("Overwrite " + newSavepointName + "?");
+        }
+
+        private void PromptForDelete()
+        {
+            PromptForConfirmation("Delete " + savepointName + "?");
+        }
+
+        private void PromptForConfirmation(string question)
+        {
+            List<string> list = new List<string>();
+            list.Add("Confirm");
+            list.Add(question);
+            list.Add("No");
+            list.Add("Yes");
+            SetSelectorData("ConfirmationSelector", list);
+            OpenDialog("ConfirmationDialog");
+        }
+
+        private void PromptPreviousState()
+        {
+            // Go back one state
+            if (state == STATE_NEWNAME)
+            {
+                PromptForSavepoint();
+            }
+            else if (state == STATE_SAVEPOINT)
+            {
+                PromptForApp();
+            }
+            else if (state == STATE_APP)
+            {
+                PromptForProfileName();
+            }
+            else
+            {
+                PromptForAction();
+            }
+        }
+
+        private void ShowPopup(string message, Action promptAction)
+        {
+            PostModeEventToModes("Evt_ShowPopup", message);
+            if (promptAction != null)
+            {
+                delay("prompt", EventType.None, 3.0, new Multimorphic.P3.VoidDelegateNoArgs(promptAction));
+            }
+        }
+
+        private bool ProfileSelectorResultEventHandler(string evtName, object evtData)
+        {
+            string selection = (string)evtData;
+            CloseDialog("ProfileDialog");
+
+            // Advance one state
+
+            if (state == STATE_ACTION)
+            {
+                action = selection;
+                PromptForProfileName();
+            }
+            else if (state == STATE_PROFILE)
+            {
+                profileName = selection;
+                PromptForApp();
+            }
+            else if (state == STATE_APP)
+            {
+                appName = selection;
+                PromptForSavepoint();
+            }
+            else if (state == STATE_SAVEPOINT)
+            {
+                savepointName = selection;
+                if (action == ACTION_DELETE)
+                {
+                    PromptForDelete();
+                }
+                else
+                {
+                    PromptForNewName();
+                }
+            }
+            return EVENT_STOP;
+        }
+
+        private bool ProfileSelectorCancelledEventHandler(string evtName, object evtData)
+        {
+            CloseDialog("ProfileDialog");
+            PromptPreviousState();
+            return EVENT_STOP;
+        }
+
+        private bool ConfirmationBoxConfirmEventHandler(string evtName, object evtData)
+        {
+            bool confirmed = (int)evtData == 1;
+
+            if (confirmed)
+            {
+                if (action == ACTION_RENAME)
+                {
+                    RenameSavepoint(true);
+                }
+                else if (action == ACTION_COPY)
+                {
+                    CopySavepoint(true);
+                }
+                else if (action == ACTION_DELETE)
+                {
+                    DeleteSavepoint();
+                }
+            }
+            else
+            {
+                // Cancel everything and go back to top menu
+                PromptForAction();
+            }
+            return EVENT_STOP;
+        }
+
+        private bool ProfileNameEntryCompletedEventHandler(string evtName, object evtData)
+        {
+            newSavepointName = ((string)evtData).Trim();
+            CloseDialog("ProfileNameEditor");
+
+            if (action == ACTION_RENAME)
+            {
+                RenameSavepoint(false);
+            }
+            else if (action == ACTION_COPY)
+            {
+                CopySavepoint(false);
+            }
+            return EVENT_STOP;
+        }
+
+        private bool ProfileNameEntryCancelledEventHandler(string evtName, object evtData)
+        {
+            CloseDialog("ProfileNameEditor");
+            PromptPreviousState();
+            return EVENT_STOP;
+        }
+
+        private void RenameSavepoint(bool confirmed)
+        {
+            string savepoint = GetSavepointPath();
+            string newSavepoint = GetNewSavepointPath();
+
+            if (CheckDestinationExists(savepoint, newSavepoint, confirmed))
+            {
+                return;
+            }
+
+            Logger.Log(LogCategories.Mode, "RenameSavepoint: " + savepoint + " To: " + newSavepoint);
+            try
+            {
+                File.Move(savepoint, newSavepoint);
+                ShowPopup("Savepoint Renamed", PromptForAction);
+                DeleteBackup();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error renaming savepoint: " + savepoint + " To: " + newSavepoint);
+                Logger.LogException(e);
+                ShowPopup("Rename Failed", PromptForAction);
+                RestoreBackup();
+            }
+        }
+
+        private void CopySavepoint(bool confirmed)
+        {
+            string savepoint = GetSavepointPath();
+            string newSavepoint = GetNewSavepointPath();
+
+            if (CheckDestinationExists(savepoint, newSavepoint, confirmed))
+            {
+                return;
+            }
+
+            Logger.Log(LogCategories.Mode, "CopySavepoint: " + savepoint + " To: " + newSavepoint);
+            try
+            {
+                File.Copy(savepoint, newSavepoint);
+                ShowPopup("Savepoint Copied", PromptForAction);
+                DeleteBackup();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error copying savepoint: " + savepoint + " To: " + newSavepoint);
+                Logger.LogException(e);
+                ShowPopup("Copy Failed", PromptForAction);
+                RestoreBackup();
+            }
+        }
+
+        private void DeleteSavepoint()
+        {
+            string savepoint = GetSavepointPath();
+            Logger.Log(LogCategories.Mode, "DeleteSavepoint: " + savepoint);
+            try
+            {
+                File.Delete(savepoint);
+                ShowPopup("Savepoint Deleted", PromptForAction);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error deleting savepoint: " + savepoint);
+                Logger.LogException(e);
+                ShowPopup("Delete Failed", PromptForAction);
+            }
+        }
+
+        // Return true if the destination exists and we need to ask the user.
+        // Return false if the operation can go ahead.
+        private bool CheckDestinationExists(string savepoint, string newSavepoint, bool confirmed)
+        {
+            if (savepoint == newSavepoint)
+            {
+                ShowPopup("Enter a different name", PromptForNewName);
+                return true;
+            }
+            if (File.Exists(newSavepoint))
+            {
+                if (!confirmed)
+                {
+                    PromptForOverwrite();
+                    return true;
+                }
+
+                // Backup the file, if the operation fails we can restore later
+                try
+                {
+                    CreateBackup(newSavepoint);
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError("Error backuping savepoint: " + newSavepoint + " To: " + backupPath);
+                    Logger.LogException(e);
+                    ShowPopup("CreateBackup Failed", PromptForAction);
+                    return true;
+                }
+            }
+
+            // the operation can continue
+            return false;
+        }
+
+        // Create a backup copy of a savepoint
+        // throws on error
+        private void CreateBackup(string newSavepoint)
+        {
+            backupPath = newSavepoint + BACKUP_EXTENSION;
+            File.Move(newSavepoint, backupPath);
+        }
+
+        // Delete the backup if there is one
+        // Never throws
+        private void DeleteBackup()
+        {
+            try
+            {
+                if (backupPath != null)
+                {
+                    File.Delete(backupPath);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error deleting backup: " + backupPath);
+                Logger.LogException(e);
+                ShowPopup("DeleteBackup Failed", null);
+                // Do not rethrow
+            }
+        }
+
+        // Restores the savepoint if there is a backup
+        // Never throws
+        private void RestoreBackup()
+        {
+            try
+            {
+                if (backupPath != null)
+                {
+                    string originalSavepoint = backupPath.Substring(0, backupPath.Length - BACKUP_EXTENSION.Length);
+                    File.Delete(originalSavepoint);
+                    File.Move(backupPath, originalSavepoint);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error restoring backup: " + backupPath);
+                Logger.LogException(e);
+                // Do not overwrite existing popup dialog
+                // Do not rethrow
+            }
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# P3SavepointManager
-A simple file manager to rename, copy or delete savepoints on the P3 pinball machine
+## P3SavepointManager
+
+[P3SavepointManager](https://github.com/clempo2/P3SavepointManager) is a simple file manager to rename, copy or delete savepoints on the [P3 pinball machine](https://www.multimorphic.com/p3-pinball-platform/) by [Multimorphic](https://www.multimorphic.com/). This functionality is missing from the P3 feature menu in the [P3 SDK V0.8](https://www.multimorphic.com/support/projects/customer-support/wiki/3rd-Party_Development_Kit)
+
+## Installation
+
+These instructions show how to modify the P3SampleApp Unity project in the P3 SDK into a P3SavepointManager Unity project. This approach is necessary to satisfy the P3 SDK license. To simplify the procedure, the minimal number of steps are given. A full distribution would remove the HomeScene and lots of unused scripts.
+
+<pre>
+  
+- Download P3_SDK_V0.8.zip from the Multimorphic support site and expand it in \P3\P3_SDK_V0.8
+  
+- Clone the P3SavepointManager repository to a local directory named P3SM:  
+    cd \P3
+    git clone https://github.com/clempo2/P3SavepointManager.git P3SM  
+
+- Copy \P3\P3_SDK_V0.8\P3SampleApp to \P3\P3SavepointManager  
+
+- Change the app code from P3SA to P3SM:  
+    Start Unity, load the project in \P3\P3SavepointManager  
+    In the top menu, select Multimorphic > Rename a P3 Project  
+        Enter New company name: Multimorphic  
+        Enter New app code: P3SM  
+        Click Continue  
+    Exit Unity
+
+- Copy the P3SavepointManager source code to the Unity project:  
+    cd \P3\P3SavepointManager  
+    Delete the \P3\P3SavepointManager\LauncherMedia directory inherited from P3SampleApp  
+    Copy \P3\P3SM\LauncherMedia to \P3\P3SavepointManager\LauncherMedia  
+    Copy \P3\P3SM\Assets\Scripts\Modes\SceneModes\P3SMAttractMode.cs to \P3\P3SavepointManager\Assets\Scripts\Modes\SceneModes\P3SMAttractMode.cs  
+
+- Edit the Attract scene:  
+    Start Unity, load the project in \P3\P3SavepointManager  
+    Select Assets > Scenes in the Project Window  
+    Double-click Attract to edit the Attract scene  
+    Select Titles in the Hierarchy window  
+        Disable the Titles: click the checkmark in the top left corner in the Inspector to remove it  
+    Select ApronDisplay in the Hierarchy window  
+        Disable the ApronDisplay: click the checkmark in the top left corner in the Inspector to remove it  
+    Select KeyIndicators in the Hierarchy window  
+        Disable the KeyIndicators: click the checkmark in the top left corner in the Inspector to remove it  
+    In the top menu, select File > Save Scenes
+
+- Repair the button legends:  
+    Edit C:\P3\P3SavepointManager\Assets\Scripts\Modes\ProfileManagement\ProfileSelectorMode.cs  
+    In the mode_started() method, remove this code:  
+            buttonLegend["LeftWhiteButton"] = "";  
+            buttonLegend["RightWhiteButton"] = "";  
+            buttonLegend["LeftRedButton"] = "Previous";  
+            buttonLegend["RightRedButton"] = "Next";  
+            buttonLegend["LeftYellowButton"] = "Exit";  
+            buttonLegend["RightYellowButton"] = "Select";  
+            buttonLegend["StartButton"] = "Select";  
+            buttonLegend["LaunchButton"] = "";  
+    Replace it with this code:  
+            buttonLegend["LeftRedButton"] = "Previous";  
+            buttonLegend["RightRedButton"] = "Next";  
+            buttonLegend["LeftYellowButton"] = "Exit";  
+            buttonLegend["RightYellowButton"] = "Select";  
+            buttonLegend["LeftWhiteButton"] = "Shift";  
+            buttonLegend["RightWhiteButton"] = "Shift";  
+            buttonLegend["StartButton"] = "Exit";  
+            buttonLegend["LaunchButton"] = "Select";  
+
+- Repair the button legends:  
+    Edit  C:\P3\P3SavepointManager\Assets\Scripts\GUI\P3SMSceneController.cs  
+    In the ShowButtonLegendEventHandler() method, locate this line  
+	    buttonLegend = (GameObject)Instantiate(Resources.Load("Prefabs/Framework/ButtonLegend"));  
+    Change "Prefabs/Framework/ButtonLegend" to "Prefabs/Framework/ButtonLegend3D"  
+
+- Run P3SavepointManager in the simulator:  
+    Select Assets > Scenes in the Project Window  
+    Double-click on Bootstrap to load the Bootstrap scene  
+    Click the Play button.  
+
+- Build the P3SavepointManager package:  
+    From the top menu, select Multimorphic > Package App For Distribution  
+    Enter Display Name: P3SavepointManager  
+    Enter Version: 1.0.0.0  
+    Enter Description: Savepoint Manager  
+    Enter Compatible Module Ids: Any  
+    Leave Feature flags and Tags empty  
+    Choose whether to make a copy on a USB drive  
+    Click Continue
+</pre>
+
+## Support
+
+Please submit a [GitHub issue](https://github.com/clempo2/P3SavepointManager/issues) if you find a problem.
+
+You can discuss P3EmptyGame and other P3 Development topics on the [P3 Community Discord Server](https://discord.gg/GuKGcaDkjd) in the dev-forum channel under the 3rd Party Development section.
+
+## License
+
+P3SavepointManager is:  
+Copyright (c) 2023 Clement Pellerin  
+MIT License.  
+Be aware this only covers the open-source portion of P3SavepointManager.
+
+The P3 SDK is:  
+Copyright (c) 2022 Multimorphic, Inc. All Rights Reserved
